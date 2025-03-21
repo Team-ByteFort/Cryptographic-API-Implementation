cd2c5aea8e7992e0033798d374ca5c1153250bbf
VijthanKurrshanth <kurrikurri0@outlook.com>
2025-03-21 22:15:14 +0530

    Refactor app initialization to register key, encrypt, and hash routes without URL prefixes

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,16 @@
 from flask import Flask
-from app.routes.key_routes import key_routes
+
 from app.routes.encrypt_routes import encrypt_routes
 from app.routes.hash_routes import hash_routes
+from app.routes.key_routes import key_routes
+
 
 def create_app():
     app = Flask(__name__)
 
     # Register Blueprints
-    app.register_blueprint(key_routes, url_prefix='/key')
-    app.register_blueprint(encrypt_routes, url_prefix='/encrypt')
-    app.register_blueprint(hash_routes, url_prefix='/hash')
+    app.register_blueprint(key_routes)
+    app.register_blueprint(encrypt_routes)
+    app.register_blueprint(hash_routes)
 
     return app
