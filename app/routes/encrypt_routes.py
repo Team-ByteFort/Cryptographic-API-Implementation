d01a42037d4d0d1d61cc783bc33f182f6bce65d3
VijthanKurrshanth <kurrikurri0@outlook.com>
2025-03-21 21:45:52 +0530

    Refactor encryption routes to include Pydantic validation and improve error handling

--- app/routes/encrypt_routes.py
+++ app/routes/encrypt_routes.py
@@ -1,21 +1,39 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, jsonify, request
+from pydantic import ValidationError
+
+from app.models.dto import EncryptionRequest
 from app.services.encryption_service import EncryptionService
-encrypt_routes = Blueprint('encrypt_routes', __name__)
 
-@encrypt_routes.route('/encrypt', methods=['POST'])
+encrypt_routes = Blueprint("encrypt_routes", __name__)
+
+
+@encrypt_routes.route("/encrypt", methods=["POST"])
 def encrypt():
-    data = request.json
-    key_id = data.get("key_id")
-    plaintext = data.get("plaintext")
-    algorithm = data.get("algorithm")
-    if algorithm == "AES" or algorithm == "RSA":
-        ciphertext  = EncryptionService.encrypt(plaintext, key_id, algorithm)
-    else:
-        return jsonify({"error": "Unsupported algorithm. Use 'AES' or 'RSA'."}), 400
-    
-    return jsonify({"ciphertext": ciphertext})
+    response = {"error": "An error occurred while processing the request."}  # Default response
+    try:
+        # Parse JSON request data and validate it with the Pydantic model
+        data = request.get_json()
+        encryption_request = EncryptionRequest(**data)
+        encryption_request = encryption_request.model_dump()
 
-@encrypt_routes.route('/decrypt', methods=['POST'])
+        # If validation passes, proceed with the data
+        key_id = encryption_request.get("key_id")
+        plaintext = encryption_request.get("plaintext")
+        algorithm = encryption_request.get("algorithm")
+
+        if algorithm == "AES" or algorithm == "RSA":
+            response = EncryptionService.encrypt(plaintext, key_id, algorithm)
+        else:
+            return jsonify({"error": "Unsupported algorithm. Use 'AES' or 'RSA'."}), 400
+
+        return response
+
+    except ValidationError as e:
+        # If validation fails, return the error details
+        return jsonify(e.errors()), 400
+
+
+@encrypt_routes.route("/decrypt", methods=["POST"])
 def decrypt():
     data = request.json
     key_id = data.get("key_id")
